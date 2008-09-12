b00a622765ec1608c8a9d6837b89b4b22e6a735d
Michael Elsdoerfer <michael@elsdoerfer.com>
2008-09-12 17:59:24 +0200

    comment and style refactor

--- pyutils/date/__init__.py
+++ pyutils/date/__init__.py
@@ -1,23 +1,24 @@
 # coding: iso-8859-1
-"""
-parserinfo classes that need to be used with the python-dateutil
+"""``parserinfo`` classes to be used with the ``python-dateutil`
 library from:
+
     http://labix.org/python-dateutil
 
-Pass them to the dateutil.parser.parse() function to modify the parsing 
-behaviour.
+Pass them to the ``dateutil.parser.parse()`` function to modify the
+parsing behaviour.
 
-Note: Versions older then 1.3 are broken when it comes to handling 
+Note: Versions older then 1.3 are broken when it comes to handling
 custom parser infos.
 """
 
 from dateutil import parser
 
-__all__ = ['GermanParserInfo', 'MultiParserInfo']
+
+__all__ = ('GermanParserInfo', 'MultiParserInfo',)
+
 
 class GermanParserInfo(parser.parserinfo):
-    """
-    Enable german dates.
+    """Enable german dates.
     """
     WEEKDAYS = [("Mo", "Montag"),
                 ("Di", "Dienstag"),
@@ -41,18 +42,16 @@ class GermanParserInfo(parser.parserinfo):
     HMS = [("h", "Stunde", "Stunden"),
            ("m", "Minute", "Minuten"),
            ("s", "Sekunde", "Sekunden")]
-    
+
     def __init__(self, dayfirst=True, yearfirst=False):
-        """
-        set dayfirst by default for German.
-        """
+        # for german dates, set ``dayfirst`` by default
         super(GermanParserInfo, self).__init__(dayfirst=dayfirst, yearfirst=yearfirst)
 
     def weekday(self, name):
         """
-        Need to reimplement this, as German weekdays in shortform are only
-        two characters long, and the superclass implementation has a hardcoded
-        requirement of at least 3.
+        We need to reimplement this, as German weekdays in shortform
+        are only two characters long, and the superclass implementation
+        has a hardcoded requirement of at least 3.
         """
         if len(name) >= 2:
             try:
@@ -61,21 +60,23 @@ class GermanParserInfo(parser.parserinfo):
                 pass
         return None
 
+
 class MultiParserInfo(parser.parserinfo):
-    """
-    Allows to combine multiple parserinfo classes, if you need/want to support
-    different languages for example. Obviously, it needs to be passed as an
-    instance, so you can put the parserinfo's to use in the constructor.
-    Otherwise, only the default parserinfo will be used.
+    """Allows to combine multiple ``parserinfo`` classes, if you
+    need/want to support different languages for example. Obviously,
+    it needs to be passed as an instance, so you can put the parserinfo's
+    to use in the constructor. Otherwise, only the default
+    parserinfo will be used.
 
     Usage:
-      MultiParserInfo(parsers=[FrenchParser, GermanParser()], param1, param2)
 
-    As you can see, you can put both instances and classes in "parsers". If you
-    pass a class, an instance will be created using the other parameters you
-    gave to MultiParserInfo(). Some functionality, that can not reasonably be
-    expanded over multiple parsers will just use the very first one, so for those
-    cases order will have a limited effect.
+        MultiParserInfo(parsers=[FrenchParser, GermanParser()], param1, param2)
+
+    As you can see, you can put both instances and classes in "parsers".
+    If you pass a class, an instance will be created using the other
+    parameters you gave to ``MultiParserInfo``. Some functionality, that
+    can not reasonably be expanded over multiple parsers will just use
+    the very first one, so for those cases order will have a limited effect.
     """
     def __init__(self, parsers=[], *args, **kwargs):
         import inspect
@@ -103,7 +104,7 @@ class MultiParserInfo(parser.parserinfo):
 
     def _try_all(self, method, args):
         """
-        Ccall a method on all the parsers and use the first return value that
+        Call a method on all the parsers and use the first return value that
         evaluates to ``True``. Otherwise, fall back to the implementation of
         our superclass (which is the default ``parserinfo``).
         """
@@ -149,12 +150,15 @@ class MultiParserInfo(parser.parserinfo):
         # does, at least), most often only the first one is actually used.
         return self._try_all("validate", [res])
 
+
 def parse_de_date(str):
-    """
-    Simple shortcut to use the dateparser with our German parserinfo class.
-    Note that it tries to convert str to iso-8859-1, as this is the encoding
-    the dateutil parser uses (as does this file), and it's required to match
-    identifiers with special characters, like "März".
+    """Simple shortcut to use the dateparser with our German
+    ``parserinfo`` class.
+
+    Note that it tries to convert str to ``iso-8859-1``, as this is
+    the encoding the dateutil parser uses (as does this file), and
+    it's required to match identifiers with special characters,
+    like "März".
     """
     return parser.parse(str.encode('iso-8859-1', 'replace'),
         parserinfo=MultiParserInfo(parsers=[GermanParserInfo(dayfirst=True)]))
